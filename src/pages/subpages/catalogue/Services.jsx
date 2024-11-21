d87890390ccea594f58d2c5df8e6958265f4c837
Rahul Yadav <rahulyad8860@gmail.com>
2024-11-21 17:43:44 +0530

    'addtable'

--- src/pages/subpages/catalogue/Services.jsx
+++ src/pages/subpages/catalogue/Services.jsx
@@ -1,14 +1,40 @@
+import React from "react";
 import Layout from "../../../layouts/Layout";
+import DynamicTable from "../../../components/DynamicTable";
+const  Services = () => {
+  const faqColumns = [
+    { key: "Name", label: "Name" },
+    { key: "Best Seller", label: "Best Seller" },
+    { key: "status", label: "Status" },
+    { key: "createdAt", label: "Created At" },
+  ];
 
-const Services = () => {
+  const faqData = [
+    {
+      id: 1,
+      Name: "Roy",
+      "Best Seller": "Yes",
+      status: "Active",
+      createdAt: "22 May 2024",
+    },
+    {
+      id: 2,
+      Name: "Roy",
+      "Best Seller": "Yes",
+      status: "Active",
+      createdAt: "22 May 2024",
+    },
+  ];
+
   return (
     <Layout>
-      <div className="bg-white shadow p-4 rounded-lg">
-        <h1 className="text-2xl font-bold">Services</h1>
-        <p className="mt-4">Welcome to the Services page!</p>
+      <div className="p-6 bg-gray-100 min-h-screen">
+        <DynamicTable title="Services" initialData={faqData} columns={faqColumns} />
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Services
+export default  Services;
+
+
