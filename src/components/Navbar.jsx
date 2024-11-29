039373e38dce38debb1804ab22bcae8420d520d6
lakshay <lakshay8219@gmail.com>
2024-11-29 10:44:15 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Navbar = ({ isAuthenticated = true }) => {
+const Navbar = ({ isAuthenticated = false }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
