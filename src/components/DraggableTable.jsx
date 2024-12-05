e167450f915f3a33ec62b405bf05453fe8e63e28
shreyaskar-1 <shreyaskar21@gmail.com>
2024-12-05 13:04:24 +0530

    changes

--- src/components/DraggableTable.jsx
+++ src/components/DraggableTable.jsx
@@ -112,7 +112,7 @@ const DraggableTable = ({ columns, columnMap, data, onReorder, onNavigate, headi
           onClick={handleNavigateClick}
           className="border-blue-600 border-2 text-black px-4 py-1 rounded-md hover:text-white hover:bg-gradient-to-r from-[#2C52A0] to-[#4189C4] flex items-center gap-1 cursor-pointer"
         >
-          Add Entry
+          Add {heading}
         </button>
       </div>
   
