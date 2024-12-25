530c6219e8f5f324cbc0ca64a27f79ddd5ea3559
Rahul Yadav <rahulyad8860@gmail.com>
2024-12-25 12:04:52 +0530

    ''

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -37,20 +37,20 @@ function Landing({ isAuthenticated = false }) {
     <div className="w-full h-full">
       <Navbar isAuthenticated={isAuthenticated} />
       {/* static content */}
-      <div className="w-full h-full flex flex-col md:flex-row gap-4 p-6">
+      <div className="w-full h-full flex border-2  border-red-700 bg-[url('https://wallpaperaccess.com/full/1137443.jpg')] bg-cover bg-center flex-col md:flex-row gap-4 p-6">
         <div className="flex flex-col items-start justify-center gap-3 md:gap-4 md:w-1/2 md:p-[3vw]">
-          <h1 className="text-3xl md:text-[4vw] font-bold md:leading-[4vw]">
+          <h1 className="text-3xl md:text-[4vw] text-gray-400 font-bold md:leading-[4vw]">
             Find Cleaning{" "}
             <span className="bg-gradient-to-r from-[#2C52A0] to-[#4189C4] bg-clip-text text-transparent">
               Services Near You
             </span>
           </h1>
-          <h2 className="text-md md:text-[2vw] font-semibold text-gray-400">
+          <h2 className="text-md md:text-[2vw] font-semibold text-gray-300">
             Explore Best Cleaning Services Near You
           </h2>
           <button
             onClick={handleClick}
-            className="text-lg md:text-[2vw] px-4 py-2 md:px-2vw] md:py-[1vw] rounded-md font-semibold text-white bg-gray-400 hover:bg-gradient-to-r from-[#2C52A0] to-[#4189C4]"
+            className="text-lg md:text-[2vw] px-4 py-2 md:px-2vw] md:py-[1vw] rounded-md font-semibold text-white bg-gray-500 hover:bg-gradient-to-r from-[#2C52A0] to-[#4189C4]"
           >
             Book Now
           </button>
@@ -59,7 +59,7 @@ function Landing({ isAuthenticated = false }) {
         <div className=" md:w-1/2 p-[3vw]">
           <img
             className="h-full w-full hover:scale-105 duration-1000 rounded-2xl object-fill shadow-xl"
-            src="https://img.freepik.com/free-photo/young-housewife-cleaning-with-rug-detergent-isolated_231208-10959.jpg?t=st=1732788504~exp=1732792104~hmac=9e6fb79ebcb02eb7359b16bf35bd315343fbd08b36e97ce9b31806bc686ed287&w=996"
+            src="https://img.freepik.com/free-photo/medium-shot-people-cleaning-building_23-2150454555.jpg?t=st=1734599132~exp=1734602732~hmac=da0d17153e3a454f617ea4451c9676456eb5ae330adc154f22faf67cdc32ebc0&w=1060"
             alt=""
           />
         </div>
