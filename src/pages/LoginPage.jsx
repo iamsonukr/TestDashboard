5292942d4a8e4ff07a781fb642f93641d6ac3655
Rahul Yadav <rahulyad8860@gmail.com>
2025-01-03 10:47:30 +0530

    'updates'

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -11,7 +11,6 @@ const LoginPage = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
-  // const navigate=useNavigate()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
